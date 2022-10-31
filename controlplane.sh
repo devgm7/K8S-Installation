2d3f709de1ef8e2fdd37625f8079da4c0d24355d
devgm7 <devops.gokulmanohar@gmail.com>
2022-10-31 15:54:23 +0530

    Update installation files

--- controlplane.sh
+++ controlplane.sh
@@ -5,6 +5,7 @@ echo "Installing Docker (docker.io)"
 sudo apt-get update && sudo apt-get install -y docker.io
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
+echo "Installing Kubernetes (kubelet kubeadm kubectl)"
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
